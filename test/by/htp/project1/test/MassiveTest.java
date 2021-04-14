1340bda518f0f6e4645b8a84234e497781dc34a1
Astreyaaa <80713425+Astreyaaa@users.noreply.github.com>
2021-04-14 13:42:53 +0300

    Add files via upload

---
+++ test/by/htp/project1/test/MassiveTest.java
@@ -0,0 +1,33 @@
+package by.htp.project1.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import by.htp.project1.logic.MassiveLogic;
+
+public class MassiveTest {
+
+	  @Test
+	  public void massiveT001() {
+		  int[] massive = new int[] {1,2,3,4,5};
+		  boolean checkReal;
+		  boolean checkExp = true;
+		  
+		  MassiveLogic logic = new MassiveLogic();
+		  
+		  checkReal = logic.check(massive);
+		  Assert.assertEquals(checkExp, checkReal);
+	  }
+	  
+	  @Test
+	  public void massiveT002() {
+		  int[] massive = new int[] {1,0,3,4,5};
+		  boolean checkReal;
+		  boolean checkExp = false;
+		  
+		  MassiveLogic logic = new MassiveLogic();
+		  
+		  checkReal = logic.check(massive);
+		  Assert.assertEquals(checkExp, checkReal);
+	  }
+	  
+}
